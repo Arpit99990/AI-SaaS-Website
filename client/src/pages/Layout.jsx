24c8b5bff66d0169ceb8705736d454ea1b9107ac
Interviewbit <arpittiwari621@gmail.com>
2025-10-06 12:28:25 +0530

    Frontpage done

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 const Layout = () => {
+
+  const navigate = useNavigate()
+
+
   return (
-    <div>
-      <h1>Layout</h1>
+    <div className='flex flex-col items-start justify-start h-screen'>
+
+      <nav>
+        <img src={assets.logo} alt="" onClick={()=>navigate('/')}/>
+        {
+          
+        }
+      </nav>
+      
       <Outlet />
     </div>
   )
